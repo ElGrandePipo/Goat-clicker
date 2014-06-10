8b8e6b45e7064967ab95a42da2b6b85a8096abd2
Arallu <damien8laporte@gmail.com>
2014-06-10 19:57:54 +0100

    si.js

---
+++ Scripts/si.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Damien on 6/10/14.
+ */
+function SysInfos(){
+    this.Banque = new Banque(20);
+    // à remplacer par tab de magasins
+    this.Magasins = new MagasinNeutre();
+
+    this.Player = new Player();
+}
+
+// commercant : dans un premier temps simple niveau
+
+// evt démonic : production augmentée, population doublée, évolution techno ou particulière, etc
