de21e6394693dae0bc63fdb37f4ba1b01c828446
Arallu <damien8laporte@gmail.com>
2014-06-10 19:59:32 +0100

    Create goat.js

---
+++ Scripts/goat.js
@@ -0,0 +1,8 @@
+function Goat(ratioProductionLait, qualiteLait, espaceConsomme) {
+    this.ratioProductionLait = ratioProductionLait;
+    this.qualiteLait = qualiteLait;
+    this.espaceConsomme = espaceConsomme;
+    //introduire type pour bonus qque (chèvre spatiale ? , etc)
+    //this.Type = typeGoat;
+}
+
