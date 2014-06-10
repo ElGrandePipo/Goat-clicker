ec00ab68d85f766a5b2cdac91a4b10714e9edf2e
Arallu <damien8laporte@gmail.com>
2014-06-10 19:59:53 +0100

    Create effet.js

---
+++ Scripts/effet.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Damien on 6/10/14.
+ */
+var EVariableEffet = {"qualiteLait":1, "ratioProductionLait":2, "espaceConsomme":3, "niveauCommercant":4};
+Object.freeze(EMorality);
+
+function Effet(enumEffet, modificateur){
+    this.Effet = enumEffet;
+    this.Modificateur = modificateur;
+}
+
+var EModificateur = {"plus":1, "fois":2};
+Object.freeze(EModificateur);
+
+function Modificateur(enumModif, valeur){
+    this.Modif = enumModif;
+    this.Valeur = valeur;
+}
