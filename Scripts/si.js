cea0ebac54b22fefad36e26cef8ca3047c6d8d94
Arallu <damien8laporte@gmail.com>
2014-06-16 05:15:56 +0100

    ajouts pour autoclic et refacto
    manque encore pour autoclic mais ajout des mécanismes notamment pour
    reprise "sauvegarde", manque gestion temps

--- Scripts/si.js
+++ Scripts/si.js
@@ -7,7 +7,26 @@ function SysInfos(){
     this.Magasins = new MagasinNeutre();
 
     this.Player = new Player();
+
+    this.GenererProductionClic = function(){
+        return this.Banque.GenererProduction(this);
+    }
+
+    // TODO prendre timestamp derniere sauvegarde pour différentiel lorsque reprise
+    // if elements lazy (G.Lutins), alors on peut appliquer un coefficient réducteur (voir si on le met pas dans la compétence) en mode "non connecté)
+    this.LastTime = 0;
+
+    this.TimeDiff = function(){
+        // return dateTime.Now - this.LastTime;
+    }
+
+    // applique le différentiel de temps * la production par seconde
+    this.ApplyAutoClics  = function(){
+        this.Player.LaitAccumule += this.TimeDiff() * this.GenererProductionClic();
+    }
 }
+
+
 
 // commercant : dans un premier temps simple niveau
 
